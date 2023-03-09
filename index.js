f698d6e7db0b657b35f7be0a1b7dac225ae6366d
vidanm <vidan.tauriff@gmail.com>
2023-03-09 20:42:13 +0100

    animation

--- index.js
+++ index.js
@@ -21,28 +21,36 @@ $(document).ready(() => {
     // used to create dynamic updating items.
     var A = point(-1,-1), B = point(1,-1), C = point(-1,1), l = circle(-1,1,0.5);
 
+		let x = -1
+		let y = -1
+		let timeout = setInterval(()=>{ x += 0.01; y+= 0.01;},100);
     // Ganja.js can directly graph 2D PGA elements. Pass in an array of
     // items to render. (numbers are colors, strings are labels, PGA points
     // and lines are rendered automatically and arrays can be used for line
     // segments and polygons). The graph function returns a HTML SVG element.
-    document.body.appendChild(this.graph([
+    document.body.appendChild(this.graph(()=>{
+			A = point(x,y);
+
+			return [
       // use numbers to set the current color.
         0x444444,
       // strings label the items they follow, first string is a title.
-        "title",
+        "Boids",
       // render points (user can drag these)
         A, "A",
-				B,"B", 
+				B, "B", 
 				C, "C",
       // render lines
-        l,"Circle",
+        l, "Circle",
       // line segments
         ()=>[A,B], "Label for segment",
       // polygons
 				()=>A^B^C, "Circle by wedge ABC",
        // 0xffeeee,
 			//()=>[A,B,C], 0xff7777, "Label for polygon"
-    ],{grid:true, animate:true,conformal:true}));
+    ]},{grid:false, animate:true,conformal:true}));
 
 	});
 }); 
+
+
